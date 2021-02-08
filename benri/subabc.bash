3dc4353377b890c129d9e055788f28182825d54b
uriuriboo <65266760+uriuriboo@users.noreply.github.com>
2021-02-08 19:43:50 +0900

    Rename benri/subabc.bash to bash/subabc.bash

--- benri/subabc.bash
+++
@@ -1,4 +0,0 @@
-ontest="abc"
-problemnnum=$1
-problemname=$2
-oj s https://atcoder.jp/contests/abc${problemnnum}/tasks/abc${problemnnum}_$problemname /mnt/c/Users/Yamaoka/kyopuro/AtCoder/ABC/${problemnnum}/${problemname}.cpp
