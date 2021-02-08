f5152598e49d0030d5c7ed9c9fbfb02e3dd0370a
uriuriboo <65266760+uriuriboo@users.noreply.github.com>
2021-02-08 19:41:09 +0900

    Add files via upload

---
+++ benri/subabc.bash
@@ -0,0 +1,4 @@
+ontest="abc"
+problemnnum=$1
+problemname=$2
+oj s https://atcoder.jp/contests/abc${problemnnum}/tasks/abc${problemnnum}_$problemname /mnt/c/Users/Yamaoka/kyopuro/AtCoder/ABC/${problemnnum}/${problemname}.cpp
