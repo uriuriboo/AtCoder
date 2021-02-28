4ecbaf24df8956157003b4a84039652c2df0f484
uriuriboo <65266760+uriuriboo@users.noreply.github.com>
2021-02-28 14:29:59 +0900

    Add files via upload

--- Library/dijkstra2.cpp
+++ Library/dijkstra2.cpp
@@ -8,15 +8,11 @@ const ll lINF = 1e18;
 template <class T> inline bool chmin(T &a, T b) {if (a > b) {a = b; return true;} return false;}
 template <class T> inline bool chmax(T &a, T b) {if (a < b) {a = b; return true;} return false;}
 
-struct Edge{
-    int to;
-    ll w;
-    Edge(int to,ll w): to(to),w(w){}
-};
+using TU = tuple<int,ll>;
 
-using Graph = vector<vector<Edge>>;
+using Graph = vector<vector<TU>>;
 //s,Gは0オリジンに直しておくこと
-vector<ll> dijkstra(int s,Graph G){
+vector<ll> dijkstra(int s,Graph &G){
     int n = G.size();
     vector<ll> dist(n,lINF);
     dist[s] = 0;
@@ -25,14 +21,19 @@ vector<ll> dijkstra(int s,Graph G){
     que.push(make_pair(dist[s],s));
 
     while(!que.empty()){
-        int v = que.top().second;
-        ll d = que.top().first;
+        int v;
+        ll d;
+        tie(d,v) = que.top();
         que.pop();
 
         if(d > dist[v]) continue;
 
-        for(auto e:G[v]){
-            if(chmin(dist[e.to],dist[v]+e.w)) que.push(make_pair(dist[e.to],e.to));
+        for(auto nxt:G[v]){
+            int nv;
+            ll w;
+            tie(nv,w) = nxt;
+            if(chmin(dist[nv],dist[v]+w))
+                que.push(make_pair(dist[nv], nv));
         }
     }
     return dist;
