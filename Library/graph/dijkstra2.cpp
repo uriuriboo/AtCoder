d042ae04ec9809190ed0823d674f353b9eb53cdd
uriuriboo <65266760+uriuriboo@users.noreply.github.com>
2021-03-05 21:28:13 +0900

    Update dijkstra2.cpp
    ダイクストラ法の関数。(辺、重み)のグラフで求める。

--- Library/graph/dijkstra2.cpp
+++ Library/graph/dijkstra2.cpp
@@ -6,7 +6,7 @@ typedef long long ll;
 const int INF = 1e9;
 const ll lINF = 1e18;
 template <class T> inline bool chmin(T &a, T b) {if (a > b) {a = b; return true;} return false;}
-template <class T> inline bool chmax(T &a, T b) {if (a < b) {a = b; return true;} return false;}
+
 
 using TU = tuple<int,ll>;
 
