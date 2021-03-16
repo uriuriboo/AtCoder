0f5ba3a1b76510778601c823a4feed7e472936a0
uriuriboo <65266760+uriuriboo@users.noreply.github.com>
2021-03-16 17:17:31 +0900

    Add files via upload

--- bash/cptest.bash
+++ bash/cptest.bash
@@ -2,7 +2,7 @@
 contest="abc"
 problemname=$1
 problem=$2
-if [ ! -e test/abc${problemname}_${problem} ];then
-    oj d https://atcoder.jp/contests/${contest}${problemname}/tasks/${contest}${problemname}_${problem} -d test/abc${problemname}_${problem}
+if [ ! -e test/${contest}${problemname}_${problem} ];then
+    oj d https://atcoder.jp/contests/${contest}${problemname}/tasks/${contest}${problemname}_${problem} -d test/${contest}${problemname}_${problem}
 fi
 oj test  -c ./problem.exe ./test/${contest}$1_$2/* 
